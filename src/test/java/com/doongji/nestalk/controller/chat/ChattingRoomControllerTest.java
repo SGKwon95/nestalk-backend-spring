e1e9cbb2f8cda3ff7fbe07227688942e165e4f35
SGKwon95 <ssunder7@naver.com>
2021-03-22 17:41:56 +0900

    test: 채팅 리스트 test code 추가

--- src/test/java/com/doongji/nestalk/controller/chat/ChattingRoomControllerTest.java
+++ src/test/java/com/doongji/nestalk/controller/chat/ChattingRoomControllerTest.java
@@ -3,31 +3,40 @@ package com.doongji.nestalk.controller.chat;
 import com.doongji.nestalk.entity.chat.Room;
 import com.doongji.nestalk.repository.user.RoomRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import javax.transaction.Transactional;
 
 @Slf4j
 @SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureMockMvc
 public class ChattingRoomControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Autowired
     private RoomRepository roomRepository;
 
     @Test
-    public void lookupChattingRoom() throws Exception{
+    @Transactional
+    public void lookupChattingRoom() throws Exception {
+        roomRepository.save(Room.builder().room_pk(1L).room_name("First Room").build());
+        roomRepository.save(Room.builder().room_pk(2L).room_name("Second Room").build());
 
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/chat/lookup")
+                .header("x-access-token", "Bearer "+System.getenv("TOKEN")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("[{\"room_pk\":1,\"room_name\":\"First Room\"},{\"room_pk\":2,\"room_name\":\"Second Room\"}]"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
