a36a61a7331334bd5385531a311b27a9540402ba
SGKwon95 <ssunder7@naver.com>
2021-04-12 02:00:54 +0900

    CI : 참여자들의 객체 정보, 참여자 수 필드 추가

--- src/main/java/com/doongji/nestalk/controller/v1/chat/dto/RoomDto.java
+++ src/main/java/com/doongji/nestalk/controller/v1/chat/dto/RoomDto.java
@@ -1,5 +1,6 @@
 package com.doongji.nestalk.controller.v1.chat.dto;
 
+import com.doongji.nestalk.entity.chat.Participant;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,10 +14,16 @@ public class RoomDto {
 
     private String name;
 
+    private List<Participant> participantList;
+
     private List<String> profileImageUrlList;
 
-    public RoomDto(String name, List<String> profileImageUrlList) {
+    private int userCnt;
+
+    public RoomDto(String name, List<Participant> participantList, List<String> profileImageUrlList, int userCnt) {
         this.name = name;
+        this.participantList = participantList;
         this.profileImageUrlList = profileImageUrlList;
+        this.userCnt = userCnt;
     }
 }
